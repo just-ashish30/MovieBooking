14e5a070588874c82ca2d76ae40cf265ecbccfe3
just-ashish30 <aashishpatel1132@gmail.com>
2026-02-04 11:20:02 +0545

    config files with meta env added

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_Key,
-  authDomain: "moviebooking-e05ee.firebaseapp.com",
-  projectId: "moviebooking-e05ee",
-  storageBucket: "moviebooking-e05ee.firebasestorage.app",
-  messagingSenderId: "102324046056",
-  appId: "1:102324046056:web:ad76d6f10a590d1e02a15d",
-  measurementId: "G-6MLSPRZ515"
+  authDomain: "import.meta.env.VITE_FIREBASE_AUTH_DOMAIN",
+  projectId: "import.meta.env.VITE_FIREBASE_PROJECT_ID",
+  storageBucket: "import.meta.env.VITE_FIREBASE_STORAGE_BUCKET",
+  messagingSenderId: "import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID",
+  appId: "import.meta.env.VITE_FIREBASE_APP_ID",
+  measurementId: "import.meta.env.VITE_FIREBASE_MEASUREMENT_ID"
 };
 
 
